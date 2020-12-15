bfdfe02743e4b81bd64ed5323d0d3f4ad28eda3c
Mythili1993 <59027192+Mythili1993@users.noreply.github.com>
2020-12-15 16:35:38 +0530

    Add files via upload

---
+++ CucumberLiveProject/JobBoardProject/ActivitiesRunner.java
@@ -0,0 +1,21 @@
+package cucumberTests;
+
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)	
+@CucumberOptions(	
+		 features = "src/test/java/Feature",
+    glue = {"stepDefinitions"},	
+    //tags = "@activity1"	
+    plugin = {"html:test-reports"},
+    monochrome = true
+	
+)
+
+public class ActivitiesRunner {
+
+}
