bec9bfe4173dca6227de28dd49f6f15d3f0d0dd2
Mythili1993 <59027192+Mythili1993@users.noreply.github.com>
2020-12-15 15:01:58 +0530

    Add files via upload

---
+++ Cucumber/TestReport/JSON Report/ActivitiesRunner1.java
@@ -0,0 +1,19 @@
+package cucumberTest;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/java/feature",
+    glue = {"stepDefinitions"},
+  //  tags = "@activity1_1",
+   monochrome = true,
+   plugin = {"json: test-reports"}
+)
+
+
+public class ActivitiesRunner1 {
+
+}
